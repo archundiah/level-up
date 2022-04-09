cfd4cba8f2758c422fe573a856436c442d72ebbc
Hugo Archundia <archundiah11@gmail.com>
2022-04-09 15:25:04 -0500

    First commit, level up created.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <>
+            <h1>Hola Mundo desde React!</h1>
+            <h2>Tienda de Videojuegos: LevelUp</h2>
+        </>
+    )
+}
+
+export default App
